8e436511abd4988db3635ca0ea9352615ee1e63a
Juliane Alves <41329356+julisevla3@users.noreply.github.com>
2022-03-21 23:25:40 -0300

    update html

--- js/script.js
+++ js/script.js
@@ -1,36 +1,3 @@
-// let container = document.getElementById('container');
-// function adicionar() {
-// let nome=document.getElementById('name').value;
-// let url=document.getElementById('imagem').value;
-// let desc=document.getElementById('descricao').value;
-// let valor=document.getElementById('valor').value;
-// let qtd=document.getElementById('quantidade').value;
-
-//     let novaDiv = document.createElement('div');
-//     novaDiv.classList.add('main-div');
-//     novaDiv.innerHTML = `
-//         <div class="card" style="width: 18rem;">
-//         <img src="${url}" class="card-img-top" alt="...">
-//             <div class="card-body">
-//                 <h2>${nome}</h2>
-//                 <p class="card-text">${desc}</p>
-//                 <p>${"R$:"+valor+",00"}</p>
-//                 <p>"Quantidade em estoque: "${qtd}</p>
-//             </div>
-//         </div>`
-
-//     //container.insertBefore(novaDiv,container.firstChild); //Os cards acabam ficando na ordem trocada
-//     //container.insertBefore(novaDiv,container.lastElementChild); //Acerta a ordem dos cards
-//     // container.insertBefore(novaDiv,container.lastChild);
-//     container.appendChild(novaDiv.lastChild); //adiciona sem quebrar a formatação do css
-//     // container.appendChild(novaDiv); 
-// };
-
-
-
-
-
-
 let container = document.getElementById('container');
 function adicionar() {
 let nome=document.getElementById('name').value;
@@ -51,10 +18,7 @@ let qtd=document.getElementById('quantidade').value;
             </div>
         </div>`
 
-    //container.insertBefore(novaDiv,container.firstChild); //Os cards acabam ficando na ordem trocada
-    //container.insertBefore(novaDiv,container.lastElementChild); //Acerta a ordem dos cards
-    // container.insertBefore(novaDiv,container.lastChild);
+   
     document.getElementById('msg').classList.add('hide');
     container.appendChild(novaDiv.lastChild); //adiciona sem quebrar a formatação do css
-    // container.appendChild(novaDiv); 
-};
+    };
